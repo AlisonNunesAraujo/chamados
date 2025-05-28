afadeba227aefd458fb3a0b5cbce1efe1e42a79f
alisondev <araujoalison304@gmail.com>
2025-05-27 21:45:39 -0300

    'ajustes'

--- src/pages/Clientes/index.js
+++ src/pages/Clientes/index.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import Header from "../../components/Header";
-import './style.css'
+import "./style.css";
 
 import { addDoc, collection } from "firebase/firestore";
 import { db } from "../../firebase/firebaseConection";
@@ -10,27 +10,23 @@ export default function Clientes() {
   const [cnpj, setCnpj] = useState("");
   const [endereço, setEndereço] = useState("");
 
-
-  async function RegistrarClientes(){
-    if(nome === '' | cnpj === '' | endereço === ''){
-        alert('digite algo')
-        return;
+  async function RegistrarClientes() {
+    if ((nome === "") | (cnpj === "") | (endereço === "")) {
+      alert("digite algo");
+      return;
     }
-    try{
-        const response = await addDoc(collection(db, 'clientes'), {
-            nome: nome,
-            cnpj: cnpj,
-            endereço: endereço,
-        })
+    try {
+      const response = await addDoc(collection(db, "clientes"), {
+        nome: nome,
+        cnpj: cnpj,
+        endereço: endereço,
+      });
 
-        alert('tudo certo')
-    }
-    catch(err){
-        alert('Algo deu errado')
+      alert("tudo certo");
+    } catch (err) {
+      alert("Algo deu errado");
     }
   }
-
-
 
   return (
     <div className="grup">
@@ -46,7 +42,7 @@ export default function Clientes() {
           placeholder="Nome da esmpresa"
           value={nome}
           onChange={(e) => setNome(e.target.value)}
-           className="inputsClientes"
+          className="inputsClientes"
         />
         <label className="titleInput">
           <h2>Cnpj</h2>
@@ -56,7 +52,7 @@ export default function Clientes() {
           placeholder="Cnpj"
           value={cnpj}
           onChange={(e) => setCnpj(e.target.value)}
-           className="inputsClientes"
+          className="inputsClientes"
         />
         <label className="titleInput">
           <h2>Endereço</h2>
@@ -69,10 +65,12 @@ export default function Clientes() {
           onChange={(e) => setEndereço(e.target.value)}
           className="inputsClientes"
         />
-        
       </form>
-      <button className="bnt" onClick={RegistrarClientes}>Salvar</button>
-      
+      <div className="areaBnt">
+        <button className="bnt" onClick={RegistrarClientes}>
+          Salvar
+        </button>
+      </div>
     </div>
   );
 }
